04b4fdbd40f5e8d28877a68a9e6a286d43c50bbd
jasonasav <jasonasav@gmail.com>
2019-05-04 14:09:06 -0700

    added webpack and babel

---
+++ app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <p>
+        I love video games
+      </p>
+      <p>- Captain PJSON</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
